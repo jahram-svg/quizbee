e1dbbf72db745cc52a4942e084bc2129cf9d50ca
jahram-svg <josephbrownson08@gmail.com>
2026-09-11 16:27:41 +0100

    Add SUPPORT_BOT_USERNAME to configuration

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 window.QUIZBEE_CONFIG = {
-    API_URL: "https://urban-lamp-r7r6vg5grp7xfgwj-5000.app.github.dev"
-};
+    API_URL: "https://urban-lamp-r7r6vg5grp7xfgwj-5000.app.github.dev",
+
+    SUPPORT_BOT_USERNAME: "QuizbeeUsersupport_bot"
+}; 
