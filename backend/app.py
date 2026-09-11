467581ad36650bc5fdbb1a8dd1c77d05223fd6e2
jahram-svg <josephbrownson08@gmail.com>
2026-09-11 23:48:29 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -13,11 +13,13 @@ from backend.firebase import db
 from backend.telegram_auth import validate_telegram_init_data
 from backend.wallet import wallet_bp
 from backend.daily_earning import daily_earning_bp
+from backend.admin import admin_bp
 
 
 app = Flask(__name__)
 app.register_blueprint(wallet_bp)
 app.register_blueprint(daily_earning_bp)
+app.register_blueprint(admin_bp)
 
 
 CORS(
