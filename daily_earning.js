7aab1695f159e33d652239d9b9e06e874b9a4ee7
jahram-svg <josephbrownson08@gmail.com>
2026-09-12 21:22:39 +0100

    Update daily_earning.js

--- daily_earning.js
+++ daily_earning.js
@@ -182,12 +182,11 @@ function renderDailyEarning() {
                         </strong>
                     </div>
 
-                    <div class="earning-help">
-                         💡 Low on points? Watch 10 ads,
-                          gain 10 points, enter Daily Earning.
-                    </div>
-
                 </div>
+
+                <div class="earning-help">
+                    💡 Low on points? Watch 10 ads, gain 10 points, enter Daily Earning.
+                </div> 
 
                 ${
                     round.end_at
