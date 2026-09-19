e8d84c2c2ab4062baee88690e8d5c371c5a36024
jahram-svg <josephbrownson08@gmail.com>
2026-09-19 22:03:20 +0100

    Refactor API_URL and add new configuration options

--- config.js
+++ config.js
@@ -1,6 +1,15 @@
 window.QUIZBEE_CONFIG = {
-    API_URL: "https://urban-lamp-r7r6vg5grp7xfgwj-5000.app.github.dev",
+
+    API_URL:
+        "https://urban-lamp-r7r6vg5grp7xfgwj-5000.app.github.dev",
 
     FUNDING_BOT_USERNAME:
-    "QuizBeeFundingBot",
-}; 
+        "QuizBeeFundingBot",
+
+    ADSGRAM_BLOCK_ID:
+        "48771",
+
+    ADSGRAM_DEBUG:
+        true
+
+};
