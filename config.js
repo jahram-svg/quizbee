b0bf30ecb28aeb4088c0247ec6c7c17ba257df59
jahram-svg <josephbrownson08@gmail.com>
2026-09-19 01:25:53 +0100

    Add FUNDING_BOT_USERNAME to config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.QUIZBEE_CONFIG = {
     API_URL: "https://urban-lamp-r7r6vg5grp7xfgwj-5000.app.github.dev",
 
-    SUPPORT_BOT_USERNAME: "QuizbeeUsersupport_bot"
+    FUNDING_BOT_USERNAME:
+    "QuizBeeFundingBot",
 }; 
